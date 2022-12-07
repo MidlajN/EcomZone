4b7e82522f1261491570b56b91496d4a1773825b
Muhammed Midlaj <midlaj4n@gmail.com>
2022-12-07 17:30:51 +0530

    E-Commerce  model & admin panel setup

---
+++ ECOM_Project/EcomApp/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    cat_img = models.ImageField(upload_to='category')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    prod_img = models.ImageField(upload_to='product')
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return '{}'.format(self.name)
