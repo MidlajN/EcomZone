ae1c45ab24dd00d49bd7b1477f0bfcf3ef0cb23a
Muhammed Midlaj <midlaj4n@gmail.com>
2022-12-12 18:43:02 +0530

    EcomApp - Paginator added

--- ECOM_Project/EcomApp/views.py
+++ ECOM_Project/EcomApp/views.py
@@ -1,18 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 
 # Create your views here.
 def allProdCat(request, c_slug=None):
     c_page = None
-    products = None
+    product_list = None
     if c_slug != None:
         c_page = get_object_or_404(Category, slug=c_slug)
-        products = Product.objects.all().filter(category=c_page, available=True)
+        product_list = Product.objects.all().filter(category=c_page, available=True)
 
     else:
-        products = Product.objects.all().filter(available=True)
+        product_list = Product.objects.all().filter(available=True)
+
+    paginator = Paginator(product_list, 6)
+    try:
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+
+    try:
+        products = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        products = paginator.page(paginator.num_pages)
 
     return render(request, 'category.html', {'category': c_page, 'products': products})
 
